c6d3657721db06a90eb561160a0a7bbb3e0d0a5e
Alptekin <alptekin.ocakdan@gmail.com>
2024-07-03 22:52:19 +0300

    Dressing the frontend

--- angular/eReconciliationsFrontEnd/src/app/components/home/home.component.ts
+++ angular/eReconciliationsFrontEnd/src/app/components/home/home.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  styleClass = "";
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  changeStyleClass(text:string) {
+    return "fixed-plugin ps "+ text;
   }
 
 }
